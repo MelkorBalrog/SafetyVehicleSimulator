a9673781c5a61626752a5db470b772203f5d8893
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-06-06 21:22:30 -0400

    Fix codegen issues in collision severity

--- Scripts/Wrappers/StabilityChecker_checkStability_wrapper.m
+++ Scripts/Wrappers/StabilityChecker_checkStability_wrapper.m
@@ -1,25 +1,13 @@
-function [dydt, accelerations] = DynamicsUpdater_stateDerivative_wrapper(stateVec)
-% DynamicsUpdater_stateDerivative_wrapper - wrapper to call DynamicsUpdater.stateDerivative for MEX
-% Usage:
-%   [dydt, accelerations] = DynamicsUpdater_stateDerivative_wrapper(stateVec)
-% Inputs:
-%   stateVec - vector [x; y; theta; p_x; p_y; L_z; phi; p]
+function [isWiggling,isRollover,isSkidding,isJackknife] = StabilityChecker_checkStability_wrapper()
+    % StabilityChecker_checkStability_wrapper - wrapper for StabilityChecker.checkStability
+    % Initializes dummy vehicle objects and returns the stability flags
 
-    persistent dyn
-    if isempty(dyn)
-        % Placeholder initialState struct
-        initialState.position = [0;0];
-        initialState.orientation = 0;
-        initialState.velocity = 0;
-        initialState.lateralVelocity = 0;
-        initialState.yawRate = 0;
-        initialState.rollAngle = 0;
-        initialState.rollRate = 0;
-        % Placeholder class instances and parameters
+    persistent checker
+    if isempty(checker)
+        % Minimal initialization of dependencies
         vehicleType = 'tractor';
         mass = 10000; friction = 0.8; velocityVec = [0;0;0];
-        dragCoeff = 0.3; airDensity = 1.225;
-        frontalArea = 10; sideArea = 20; sideForceCoeff = 1.0;
+        dragCoeff = 0.3; airDensity = 1.225; frontalArea = 10; sideArea = 20; sideForceCoeff = 1.0;
         turnRadius = Inf; loadDist = [0,0,0,mass*9.81,1]; cog = [0;0;0];
         h_CoG = 1.0; angularVel = [0;0;0]; slopeAngle = 0;
         trackWidth = 2.0; wheelbase = 3.5;
@@ -29,13 +17,14 @@ function [dydt, accelerations] = DynamicsUpdater_stateDerivative_wrapper(stateVe
         numTrailerTires = 0; trailerBoxMasses = [];
         tireModelFlag = 'simple'; highFidelityModel = [];
         windVector = [0;0;0]; brakeSystem = BrakeSystem();
+        varargin = {};
         fc = ForceCalculator(vehicleType, mass, friction, velocityVec, ...
             dragCoeff, airDensity, frontalArea, sideArea, sideForceCoeff, ...
             turnRadius, loadDist, cog, h_CoG, angularVel, slopeAngle, ...
             trackWidth, wheelbase, tireModel, suspensionModel, trailerInertia, ...
             dt, trailerMass, trailerWheelbase, numTrailerTires, trailerBoxMasses, ...
-            tireModelFlag, highFidelityModel, windVector, brakeSystem);
-        kin = KinematicsCalculator(fc, 1.0, 2.0, 200000, 5000, 5000, 150000, 4000, 8000, 0.01);
+            tireModelFlag, highFidelityModel, windVector, brakeSystem, varargin{:});
+        kin = KinematicsCalculator(fc, 1.0, 2.0, 200000, 5000, 5000, 150000, 4000, 8000, dt);
         clutch = Clutch(500, 0.5, 0.5);
         maxGear = 5;
         gearRatios = [3.5, 2.1, 1.4, 1.0, 0.8];
@@ -48,7 +37,17 @@ function [dydt, accelerations] = DynamicsUpdater_stateDerivative_wrapper(stateVe
         transmission = Transmission(maxGear, gearRatios, finalDriveRatio, ...
             shiftUpSpeed, shiftDownSpeed, engineBrakeTorque, shiftDelay, ...
             clutch, filterWindowSize);
-        dyn = DynamicsUpdater(fc, kin, initialState, 10000, 3.5, 1.0, 2.0, 0.01, 'tractor', 2.0, 5000, 500, transmission, 0.1);
+        initialState.position = [0;0];
+        initialState.orientation = 0;
+        initialState.velocity = 0;
+        initialState.lateralVelocity = 0;
+        initialState.yawRate = 0;
+        initialState.rollAngle = 0;
+        initialState.rollRate = 0;
+        dyn = DynamicsUpdater(fc, kin, initialState, mass, wheelbase, h_CoG, trackWidth, dt, vehicleType, trackWidth, 5000, 500, transmission, 0.1);
+        checker = StabilityChecker(dyn, fc, kin, 0, 0, friction, h_CoG);
     end
-    [dydt, accelerations] = dyn.stateDerivative(stateVec);
+    checker = checker.checkStability();
+    [isWiggling,isRollover,isSkidding,isJackknife] = checker.getStabilityFlags();
 end
+
