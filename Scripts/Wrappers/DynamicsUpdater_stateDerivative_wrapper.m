9009a532e04aa7eff3daf9752ba60f7f2fcd72ef
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-06-06 21:04:18 -0400

    Initialize highFidelity tire model with dummy Pacejka object

--- Scripts/Wrappers/DynamicsUpdater_stateDerivative_wrapper.m
+++ Scripts/Wrappers/DynamicsUpdater_stateDerivative_wrapper.m
@@ -17,7 +17,25 @@ function [dydt, accelerations] = DynamicsUpdater_stateDerivative_wrapper(stateVe
         initialState.rollAngle = 0;
         initialState.rollRate = 0;
         % Placeholder class instances and parameters
-        fc = ForceCalculator('tractor', 10000, 0.8, [0;0;0], 0.3, 1.225, 10, 20, 1.0, Inf, [0,0,0,10000*9.81,1], [0;0;0], 1.0, [0;0;0], 0, 2.0, 3.5, struct(), [], 0, 0.01, 0, 0, 0, 'simple', [], [0;0;0], []);
+        vehicleType = 'tractor';
+        mass = 10000; friction = 0.8; velocityVec = [0;0;0];
+        dragCoeff = 0.3; airDensity = 1.225;
+        frontalArea = 10; sideArea = 20; sideForceCoeff = 1.0;
+        turnRadius = Inf; loadDist = [0,0,0,mass*9.81,1]; cog = [0;0;0];
+        h_CoG = 1.0; angularVel = [0;0;0]; slopeAngle = 0;
+        trackWidth = 2.0; wheelbase = 3.5;
+        tireModel = struct('B',10,'C',1.9,'D',1.0,'E',0.97);
+        suspensionModel = [];
+        trailerInertia = 0; dt = 0.01; trailerMass = 0; trailerWheelbase = 0;
+        numTrailerTires = 0; trailerBoxMasses = [];
+        tireModelFlag = 'simple'; highFidelityModel = [];
+        windVector = [0;0;0]; brakeSystem = BrakeSystem();
+        fc = ForceCalculator(vehicleType, mass, friction, velocityVec, ...
+            dragCoeff, airDensity, frontalArea, sideArea, sideForceCoeff, ...
+            turnRadius, loadDist, cog, h_CoG, angularVel, slopeAngle, ...
+            trackWidth, wheelbase, tireModel, suspensionModel, trailerInertia, ...
+            dt, trailerMass, trailerWheelbase, numTrailerTires, trailerBoxMasses, ...
+            tireModelFlag, highFidelityModel, windVector, brakeSystem);
         kin = KinematicsCalculator(fc, 1.0, 2.0, 200000, 5000, 5000, 150000, 4000, 8000, 0.01);
         transmission = Transmission();
         dyn = DynamicsUpdater(fc, kin, initialState, 10000, 3.5, 1.0, 2.0, 0.01, 'tractor', 2.0, 5000, 500, transmission, 0.1);
