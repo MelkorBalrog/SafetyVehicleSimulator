14effad3449d5f06610f6fdb24fd9364cb5460e3
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-06-08 21:37:35 -0400

    Revert "Improve trailer stability control"

--- Source/Control/purePursuit_PathFollower.m
+++ Source/Control/purePursuit_PathFollower.m
@@ -266,12 +266,6 @@ classdef purePursuit_PathFollower
 
             % Set lateral acceleration limit
             obj.maxLateralAccel = maxLateralAccel;
-
-            % Pre-compute curvature and steering plan so other modules can
-            % safely query radiusOfCurvature before any predictions are
-            % provided.
-            obj = obj.calculateCurvature();
-            obj = obj.planSteeringAngles();
         end
 
         %% Update Vehicle State
